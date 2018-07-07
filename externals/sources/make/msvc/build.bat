033e4253435386f16e6d401fee92d27e6bbca89c
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-07-07 15:20:51 +0100

    Ignoring only the root build folder.

--- externals/sources/make/msvc/build.bat
+++ externals/sources/make/msvc/build.bat
@@ -15,6 +15,7 @@ if "%1"=="clean" (
         echo %%f
         RD /Q /S %%f\Debug
         RD /Q /S %%f\Release
+        RD /Q /S %%f\build
     )
 ) else (
     msbuild build.sln /p:Configuration=Debug
