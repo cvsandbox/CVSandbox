eb73a5ad93ef4f5f4aed7aab509e2499fb1528d0
Andrew Kirillov <andrew.kirillov@gmail.com>
2019-02-05 23:18:48 +0000

    Reworked build scripts for cvsandboxtools.dll and cvsandbox.exe so those follow common pattern.

--- sources/apps/cvsandboxtools/make/mingw/build.bat
+++ sources/apps/cvsandboxtools/make/mingw/build.bat
@@ -1,33 +1,4 @@
 @echo off
-
-set PATH=%PATH%;%MINGW_BIN%
-
-set BUILD_FOLDER=out_make
-set PRO_FOLDER=..\..
-set PRO_NAME=cvsandboxtools.pro
-
-if "%1"=="clean" (
-    echo "Cleaning cvsandboxtools.dll build ..."
-    
-    rd /S /Q %BUILD_FOLDER%
-    
-    goto EOF
-)
-
-if "%MINGW_BIN%"=="" (
-    echo "Cannot build cvsandboxtools.dll because MinGW binary folder is not set (MINGW_BIN)."
-    goto EOF
-)
-
-if "%QT_MINGW_BIN%"=="" (
-    echo "Cannot build cvsandboxtools.dll because Qt's MinGW binary folder is not set (QT_MINGW_BIN)."
-    goto EOF
-)
-
-mkdir %BUILD_FOLDER%
-cd %BUILD_FOLDER%
-    
-%QT_MINGW_BIN%\qmake.exe ..\%PRO_FOLDER%\%PRO_NAME%
-%MINGW_BIN%\mingw32-make.exe -f Makefile.Release
-
-:EOF
+call make.bat clean
+call make.bat
+call make.bat clean
