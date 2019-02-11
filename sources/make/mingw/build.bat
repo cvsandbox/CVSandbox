f04180adc12fe9b7c0bb4cf23f898a31aa0fdaf2
Andrew Kirillov <andrew.kirillov@gmail.com>
2019-02-11 15:05:17 +0000

    MinGW build script also copies required MinGW and Qt DLLs into the output folder.

--- sources/make/mingw/build.bat
+++ sources/make/mingw/build.bat
@@ -1,5 +1,6 @@
 @echo off
 
+@rem  Build CVSandbox source code
 set BUILD_FOLDERS=..\..\afx\make\mingw ^
                   ..\..\core\make\mingw ^
                   ..\..\plugins\make\mingw ^
@@ -9,3 +10,19 @@ set BUILD_FOLDERS=..\..\afx\make\mingw ^
 set MY_FOLDER=%cd%
 
 FOR %%I IN (%BUILD_FOLDERS%) DO cd %%I && call build.bat && cd %MY_FOLDER%
+
+@rem  Copy MinGW libraries
+set TO_COPY=libgcc_s_dw2-1.dll libstdc++-6.dll libwinpthread-1.dll libgomp-1.dll
+for %%F in (%TO_COPY%) do (
+    xcopy /Y "%MINGW_BIN%\%%F" ..\..\..\build\mingw\release\bin\
+)
+
+@rem  Copy Qt libraries
+set TO_COPY=Qt5Core.dll Qt5Gui.dll Qt5Widgets.dll
+for %%F in (%TO_COPY%) do (
+    xcopy /Y "%QT_MINGW_BIN%\%%F" ..\..\..\build\mingw\release\bin\
+)
+
+@rem  Copy Qt platform plug-in
+mkdir ..\..\..\build\mingw\release\bin\platforms
+xcopy /Y "%QT_MINGW_BIN%\..\plugins\platforms\qwindows.dll" ..\..\..\build\mingw\release\bin\platforms\
