9467b71c26519dd90dee1a1cd5e26571039fdd22
Andrew Kirillov <andrew.kirillov@gmail.com>
2019-03-08 20:54:33 +0000

    Minor clean-up in test scripts.

--- sources/plugins/devices/dev_com/examples/serial.lua
+++ sources/plugins/devices/dev_com/examples/serial.lua
@@ -7,12 +7,12 @@
     connected over serial port.
 ]]--
 
-local string = require "string"
+local string = require 'string'
 
 serialPort = Host.CreatePluginInstance( 'SerialPort' )
 serialPort:SetProperty( 'portName', 'COM8' )
 
--- Use blocking input, read operation wait up to configured
+-- Use blocking input, read operations wait up to the configured
 -- timeout value
 serialPort:SetProperty( 'blockingInput', true )
 -- Total Read Timeout = ioTimeoutConstant + ioTimeoutMultiplier * bytesRequested
@@ -20,19 +20,19 @@ serialPort:SetProperty( 'ioTimeoutConstant', 50 )
 serialPort:SetProperty( 'ioTimeoutMultiplier', 0 )
 
 function Main()
-    
+
     if serialPort:Connect( ) then
-        print('Connected')
-        
+        print( 'Connected' )
+
         -- Test IsConnected() method
-        print('IsConnected: ' .. tostring( serialPort:IsConnected( ) ))
+        print( 'IsConnected: ' .. tostring( serialPort:IsConnected( ) ) )
 
         -- Let Arduino board reset and get ready
-        sleep(1500)
-        
+        sleep( 1500 )
+
         -- Switch LED on - send command as string
         sent, status = serialPort:WriteString( 'led_on\n' )
-        
+
         print( 'status: ' .. tostring( status ) )
         print( 'sent  : ' .. tostring( sent ) )
 
@@ -41,23 +41,23 @@ function Main()
         print( 'status  : ' .. tostring( status ) )
         print( 'str read: ' .. strRead )
 
-        -- Switch LED off - sned command as table of bytes 
+        -- Switch LED off - sned command as table of bytes
         sent, status = serialPort:Write( { 0x6C, 0x65, 0x64, 0x5F, 0x6F, 0x66, 0x66, 0x0A } )
-        
+
         print( 'status: ' .. tostring( status ) )
         print( 'sent  : ' .. tostring( sent ) )
 
         readBuffer, status = serialPort:Read( 10 )
-        
+
         print( 'status    : ' .. tostring( status ) )
         print( 'bytes read: ' )
         for i=1, #readBuffer do
             print( '[', i, ']=', readBuffer[i] )
         end
-        
+
         -- Check button state
         sent, status = serialPort:WriteString( 'btn_state\n' )
-        
+
         print( 'status: ' .. tostring( status ) )
         print( 'sent  : ' .. tostring( sent ) )
 
@@ -65,7 +65,7 @@ function Main()
 
         print( 'status  : ' .. tostring( status ) )
         print( 'str read: ' .. strRead )
-        
+
         if string.sub( strRead, 1, 1 ) == '1' then
             print( 'button is ON' )
         else
@@ -78,7 +78,7 @@ function Main()
 
         print( 'status  : ' .. tostring( status ) )
         print( 'str read: ' .. strRead )
-        
+
         serialPort:Disconnect( )
     end
 end
--- sources/plugins/devices/dev_gamepad/examples/gamepad.lua
+++ sources/plugins/devices/dev_gamepad/examples/gamepad.lua
@@ -6,11 +6,12 @@
     Sample script which demonstrates how to talk
     to gamapad device conneted to the system.
 ]]--
+
 local math = require 'math'
 
 gamepad = Host.CreatePluginInstance( 'Gamepad' )
 
--- Connected to the first gamapad device 
+-- Connected to the first gamapad device
 gamepad:SetProperty( 'deviceId', 0 )
 if not gamepad:Connect( ) then
     error( "Failed connecting to gamepad" )
@@ -25,19 +26,19 @@ function Main( )
     -- Query value of all axes and buttons as arrays
     axesValues   = gamepad:GetProperty( 'axesValues' )
     buttonsState = gamepad:GetProperty( 'buttonsState' )
-    
+
     print( 'X: ' .. tostring( math.floor( axesValues[1] * 100 ) / 100 ) )
     print( 'Y: ' .. tostring( math.floor( axesValues[2] * 100 ) / 100 ) )
 
     -- Query value of the X axis only
     x = gamepad:GetProperty( 'axesValues', 1 )
-    
+
     -- Query status of the first button only
     buttonState1 = gamepad:GetProperty( 'buttonsState', 1 )
-    
+
     if buttonState1 then
-        print( "Button 1 is ON" )
+        print( 'Button 1 is ON' )
     else
-        print( "Button 1 is OFF" )
+        print( 'Button 1 is OFF' )
     end
 end
