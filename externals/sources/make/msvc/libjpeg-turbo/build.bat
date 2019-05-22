ba2ffb2c0cf59e67f5d9b1f93342f0e52d3ddda1
Andrew Kirillov <andrew.kirillov@gmail.com>
2019-05-22 15:43:23 +0100

    Added 64-bit build for libjpeg-turbo.

--- externals/sources/make/msvc/libjpeg-turbo/build.bat
+++ externals/sources/make/msvc/libjpeg-turbo/build.bat
@@ -1,11 +1,13 @@
-    @echo off
+@echo off
 
 set OUT_FOLDER=..\..\..\..\..\build\msvc
-set BUILD_FOLDER=build
 set SRC_FOLDER=..\..\..\libjpeg-turbo
+set BUILD_FOLDER=build
+set BUILD_FOLDER64=build64
 
 if "%1"=="clean" (
     rd /S /Q %BUILD_FOLDER%
+    rd /S /Q %BUILD_FOLDER64%
 ) else (
     set MY_PATH=%cd%
 
@@ -45,4 +47,45 @@ if "%1"=="clean" (
     xcopy /Y "%SRC_FOLDER%\jpeglib.h" "%OUT_DBG%\include\"
     xcopy /Y "%SRC_FOLDER%\jmorecfg.h" "%OUT_DBG%\include\"
     xcopy /Y "%SRC_FOLDER%\jerror.h" "%OUT_DBG%\include\"
+    
+    @rem 64-bit build
+
+    mkdir %BUILD_FOLDER64%
+    pushd %BUILD_FOLDER64%
+
+    cmake -G "Visual Studio 14 2015 Win64" -D ENABLE_SHARED:BOOL=ON ^
+        -D WITH_ARITH_DEC:BOOL=ON ^
+        -D WITH_ARITH_ENC:BOOL=ON ^
+        -D WITH_JPEG7:BOOL=ON ^
+        -D WITH_JPEG8:BOOL=ON ^
+        -D WITH_MEM_SRCDST:BOOL=ON ^
+        -D WITH_SIMD:BOOL=ON ^
+        -D WITH_TURBOJPEG:BOOL=ON ^
+        %MY_PATH%\%SRC_FOLDER%
+
+    msbuild libjpeg-turbo.sln /p:Configuration=Debug
+    msbuild libjpeg-turbo.sln /p:Configuration=Release
+
+    popd
+    
+    set OUT_REL64=%OUT_FOLDER%\release64
+    set OUT_DBG64=%OUT_FOLDER%\debug64
+
+    xcopy /Y "%BUILD_FOLDER64%\Release\jpeg8.dll" "%OUT_REL64%\bin\"
+    xcopy /Y "%BUILD_FOLDER64%\Release\jpeg.lib" "%OUT_REL64%\lib\"
+
+    xcopy /Y "%BUILD_FOLDER64%\Debug\jpeg8.dll" "%OUT_DBG64%\bin\"
+    xcopy /Y "%BUILD_FOLDER64%\Debug\jpeg.lib" "%OUT_DBG64%\lib\"
+
+    xcopy /Y "%BUILD_FOLDER64%\jconfig.h" "%OUT_REL64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jpeglib.h" "%OUT_REL64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jmorecfg.h" "%OUT_REL64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jerror.h" "%OUT_REL64%\include\"
+
+    xcopy /Y "%BUILD_FOLDER64%\jconfig.h" "%OUT_DBG64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jpeglib.h" "%OUT_DBG64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jmorecfg.h" "%OUT_DBG64%\include\"
+    xcopy /Y "%SRC_FOLDER%\jerror.h" "%OUT_DBG64%\include\"    
 )
+
+:EOF
