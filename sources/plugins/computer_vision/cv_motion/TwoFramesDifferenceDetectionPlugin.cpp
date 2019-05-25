113cdae3a82ba07a70902d66a7dc84807391169f
Andrew Kirillov <andrew.kirillov@gmail.com>
2019-05-25 11:09:36 +0100

    Implemented the logic of simple motion detection plug-in based on difference of two consecutive video frames. Works fine from scripting for now. Need to extend video processing graph to support detection plug-ins and running sub-graph when detection has triggered (so it could be done from UI without scripting needs).

--- sources/plugins/computer_vision/cv_motion/TwoFramesDifferenceDetectionPlugin.cpp
+++ sources/plugins/computer_vision/cv_motion/TwoFramesDifferenceDetectionPlugin.cpp
@@ -20,9 +20,9 @@
 */
 
 #include "TwoFramesDifferenceDetectionPlugin.hpp"
+#include <ximaging.h>
 
 using namespace std;
-
 
 // List of supported pixel formats
 const XPixelFormat TwoFramesDifferenceDetectionPlugin::supportedPixelFormats[] =
@@ -36,9 +36,25 @@ namespace Private
     class TwoFramesDifferenceDetectionPluginData
     {
     public:
+        ximage*     PreviousFrame;
+        ximage*     DiffImage;
+
+        int16_t     PixelThreshold;
+        float       MotionThreshold;
+        float       MotionLevel;
+
+        xargb       HiColor;
+        xargb       LowColor;
+        bool        HighlightMotion;
+        float       HighlightAmount;
 
     public:
-        TwoFramesDifferenceDetectionPluginData( ) { }
+        TwoFramesDifferenceDetectionPluginData( ) :
+            PreviousFrame( nullptr ), DiffImage( nullptr ),
+            PixelThreshold( 60 ), MotionThreshold( 0.1f ), MotionLevel( 0.0f ),
+            HiColor( { 0xFFFF0000 } ), LowColor( { 0xFF000000 } ),
+            HighlightMotion( false ), HighlightAmount( 0.3f )
+        { }
     };
 }
 
@@ -65,8 +81,38 @@ XErrorCode TwoFramesDifferenceDetectionPlugin::GetProperty( int32_t id, xvariant
 
     switch ( id )
     {
+    case 0:
+        value->type       = XVT_I2;
+        value->value.sVal = mData->PixelThreshold;
+        break;
+
+    case 1:
+        value->type       = XVT_R4;
+        value->value.fVal = mData->MotionThreshold;
+        break;
+
+    case 2:
+        value->type       = XVT_R4;
+        value->value.fVal = mData->MotionLevel;
+        break;
+
+    case 3:
+        value->type          = XVT_Bool;
+        value->value.boolVal = mData->HighlightMotion;
+        break;
+
+    case 4:
+        value->type          = XVT_Any;
+        value->value.argbVal = mData->HiColor;
+        break;
+
+    case 5:
+        value->type        = XVT_U2;
+        value->value.usVal = static_cast<uint16_t>( mData->HighlightAmount * 100 );
+
     default:
         ret = ErrorInvalidProperty;
+        break;
     }
 
     return ret;
@@ -81,10 +127,40 @@ XErrorCode TwoFramesDifferenceDetectionPlugin::SetProperty( int32_t id, const xv
     XVariantInit( &convertedValue );
 
     // make sure property value has expected type
-    // ret = PropertyChangeTypeHelper( id, value, propertiesDescription, 11, &convertedValue );
+    ret = PropertyChangeTypeHelper( id, value, propertiesDescription, 6, &convertedValue );
 
     if ( ret == SuccessCode )
     {
+        switch ( id )
+        {
+        case 0:
+            mData->PixelThreshold = convertedValue.value.sVal;
+            break;
+
+        case 1:
+            mData->MotionThreshold = convertedValue.value.fVal;
+            break;
+
+        case 2:
+            ret = ErrorReadOnlyProperty;
+            break;
+
+        case 3:
+            mData->HighlightMotion = convertedValue.value.boolVal;
+            break;
+
+        case 4:
+            mData->HiColor = convertedValue.value.argbVal;
+            break;
+
+        case 5:
+            mData->HighlightAmount = static_cast<float>( convertedValue.value.usVal ) / 100.0f;
+            break;
+
+        default:
+            ret = ErrorInvalidProperty;
+            break;
+        }
     }
 
     XVariantClear( &convertedValue );
@@ -95,7 +171,7 @@ XErrorCode TwoFramesDifferenceDetectionPlugin::SetProperty( int32_t id, const xv
 // Check if the plug-in does changes to input video frames or not
 bool TwoFramesDifferenceDetectionPlugin::IsReadOnlyMode( )
 {
-    return true;
+    return ( !mData->HighlightMotion );
 }
 
 // Get pixel formats supported by the plug-in
@@ -107,7 +183,61 @@ XErrorCode TwoFramesDifferenceDetectionPlugin::GetSupportedPixelFormats( XPixelF
 // Process the specified video frame
 XErrorCode TwoFramesDifferenceDetectionPlugin::ProcessImage( ximage* src )
 {
-    XErrorCode ret = SuccessCode;
+    XErrorCode ret = ErrorFailed;
+
+    // reset previous detected motion level
+    mData->MotionLevel = 0.0f;
+
+    if ( src == nullptr )
+    {
+        ret = ErrorNullParameter;
+    }
+    else
+    {
+        if ( mData->PreviousFrame == nullptr )
+        {
+            ret = XImageClone( src, &mData->PreviousFrame );
+        }
+        else
+        {
+            if ( ( mData->PreviousFrame->width  != src->width  ) ||
+                 ( mData->PreviousFrame->height != src->height ) ||
+                 ( mData->PreviousFrame->format != src->format ) )
+            {
+                // should not happen, but better be ready
+                Reset( );
+                ret = XImageClone( src, &mData->PreviousFrame );
+            }
+            else
+            {
+                ret = XImageClone( src, &mData->DiffImage );
+
+                if ( ret == SuccessCode )
+                {
+                    uint32_t diffPixels;
+
+                    ret = DiffImagesThresholded( mData->DiffImage, mData->PreviousFrame, mData->PixelThreshold,
+                                                 &diffPixels, mData->HiColor, mData->LowColor );
+
+                    if ( ret == SuccessCode )
+                    {
+                        mData->MotionLevel = ( 100.0f * diffPixels ) / ( src->width * src->height );
+                    }
+                }
+
+                if ( ret == SuccessCode )
+                {
+                    // update the previous frame for the next round
+                    ret = XImageClone( src, &mData->PreviousFrame );
+                }
+
+                if ( ( ret == SuccessCode ) && ( mData->HighlightMotion ) )
+                {
+                    ret = AddImages( src, mData->DiffImage, mData->HighlightAmount );
+                }
+            }
+        }
+    }
 
     return ret;
 }
@@ -115,10 +245,14 @@ XErrorCode TwoFramesDifferenceDetectionPlugin::ProcessImage( ximage* src )
 // Check if the plug-in triggered detection on the last processed image
 bool TwoFramesDifferenceDetectionPlugin::Detected( )
 {
-    return false;
+    return ( mData->MotionLevel >= mData->MotionThreshold );
 }
 
 // Reset run time state of the video processing plug-in
 void TwoFramesDifferenceDetectionPlugin::Reset( )
 {
+    XImageFree( &mData->PreviousFrame );
+    XImageFree( &mData->DiffImage );
+
+    mData->MotionLevel = 0.0f;
 }
--- sources/plugins/computer_vision/cv_motion/TwoFramesDifferenceDetectionPluginDescriptor.cpp
+++ sources/plugins/computer_vision/cv_motion/TwoFramesDifferenceDetectionPluginDescriptor.cpp
@@ -23,6 +23,7 @@
 #include "TwoFramesDifferenceDetectionPlugin.hpp"
 
 static void PluginInitializer( );
+static XErrorCode UpdateMotionHighlightProperty( PropertyDescriptor* desc, const xvariant* parentValue );
 
 // Version of the plug-in
 static xversion PluginVersion = { 1, 0, 0 };
@@ -30,14 +31,26 @@ static xversion PluginVersion = { 1, 0, 0 };
 // ID of the plug-in
 static xguid PluginID = { 0xAF000003, 0x00000000, 0x00000020, 0x00000001 };
 
-// Dummy property
-static PropertyDescriptor dummyProperty =
-{ XVT_U4, "Dummy", "dummy", "Dummy property, todo.", PropertyFlag_None };
+// Plug-in properties
+static PropertyDescriptor pixelThresholdProperty =
+{ XVT_I2, "Pixel Threshold", "pixelThreshold", "Threshold for pixel deference to treat as significant change.", PropertyFlag_None };
+static PropertyDescriptor motionThresholdProperty =
+{ XVT_R4, "Motion Threshold", "motionThreshold", "Percentage of changed pixels to trigger motion detection.", PropertyFlag_None };
+static PropertyDescriptor motionLevelProperty =
+{ XVT_R4, "Motion Level", "motionLevel", "Actual percentage of changed pixels.", PropertyFlag_ReadOnly };
+
+static PropertyDescriptor highlightMotionProperty =
+{ XVT_Bool, "Highlight Motion", "highlightMotion", "Highlight motion areas or not.", PropertyFlag_None };
+static PropertyDescriptor highlightColoProperty =
+{ XVT_ARGB, "Highlight Color", "highlightColor", "Color used to highlight motion areas.", PropertyFlag_Dependent };
+static PropertyDescriptor highlightAmountProperty =
+{ XVT_U2, "Highlight Amount", "highlightAmount", "Amount off difference map to add to source image for motion highlighting, %.", PropertyFlag_Dependent };
 
 // Array of available properties
 static PropertyDescriptor* pluginProperties[] =
 {
-    &dummyProperty
+    &pixelThresholdProperty, &motionThresholdProperty, &motionLevelProperty,
+    &highlightMotionProperty, &highlightColoProperty, &highlightAmountProperty
 };
 
 // Let the class itself know description of its properties
@@ -73,4 +86,69 @@ REGISTER_CPP_PLUGIN_WITH_PROPS
 // initialized during properties array declaration
 static void PluginInitializer( )
 {
+    // Pixel Threshol
+    pixelThresholdProperty.DefaultValue.type = XVT_I2;
+    pixelThresholdProperty.DefaultValue.value.sVal = 60;
+
+    pixelThresholdProperty.MinValue.type = XVT_I2;
+    pixelThresholdProperty.MinValue.value.sVal = 1;
+
+    pixelThresholdProperty.MaxValue.type = XVT_I2;
+    pixelThresholdProperty.MaxValue.value.sVal = 765;
+
+    // Motion Threshol
+    motionThresholdProperty.DefaultValue.type = XVT_R4;
+    motionThresholdProperty.DefaultValue.value.fVal = 0.1f;
+
+    motionThresholdProperty.MinValue.type = XVT_R4;
+    motionThresholdProperty.MinValue.value.fVal = 0.01f;
+
+    motionThresholdProperty.MaxValue.type = XVT_R4;
+    motionThresholdProperty.MaxValue.value.fVal = 50.0f;
+
+    // Highlight Motion
+    highlightMotionProperty.DefaultValue.type = XVT_Bool;
+    highlightMotionProperty.DefaultValue.value.boolVal = false;
+
+    // Highlight Color
+    highlightMotionProperty.DefaultValue.type = XVT_ARGB;
+    highlightMotionProperty.DefaultValue.value.argbVal.argb = 0xFFFF0000;
+
+    highlightMotionProperty.ParentProperty = 3;
+    highlightMotionProperty.Updater = UpdateMotionHighlightProperty;
+
+    // Highlight Amount
+    highlightAmountProperty.DefaultValue.type = XVT_U2;
+    highlightAmountProperty.DefaultValue.value.usVal = 30;
+
+    highlightAmountProperty.MinValue.type = XVT_U2;
+    highlightAmountProperty.MinValue.value.usVal = 10;
+
+    highlightAmountProperty.MaxValue.type = XVT_U2;
+    highlightAmountProperty.MaxValue.value.usVal = 100;
+
+    highlightAmountProperty.ParentProperty = 3;
+    highlightAmountProperty.Updater = UpdateMotionHighlightProperty;
+}
+
+static XErrorCode UpdateMotionHighlightProperty( PropertyDescriptor* desc, const xvariant* parentValue )
+{
+    XErrorCode ret = ErrorFailed;
+    bool       boolParentValue;
+
+    ret = XVariantToBool( parentValue, &boolParentValue );
+
+    if ( ret == SuccessCode )
+    {
+        if ( boolParentValue )
+        {
+            desc->Flags &= ( ~PropertyFlag_Disabled );
+        }
+        else
+        {
+            desc->Flags |= PropertyFlag_Disabled;
+        }
+    }
+
+    return ret;
 }
